d8ce999eaa075ba96142da772458a3fc00842d2e
Humbah2005 <tanakapunungwe21@gmail.com>
2025-05-16 11:36:07 +0200

    Update Paste.js

--- Paste.js
+++ Paste.js
@@ -1,7 +1,7 @@
 // src/main.js
 const fs = require('fs');
 const path = require('path');
-const PASTEBIN_API_KEY = process.env.PASTEBIN_API_KEY; // Replace with your Pastebin API key
+const PASTEBIN_API_KEY = process.env.PASTEBIN_API_KEY; 'mKoIZabR0uHqg1PCZl-Kc7M9bzSYnlo6'// Replace with your Pastebin API key
 
 /**
  * Uploads content to Pastebin, handling different input types like text, files, and base64 data.
